f145e27a862c95fd6c617220917b842c97b0b5f6
evlachs <evlach_e@mail.ru>
2023-12-14 20:27:10 +0300

    visualization updated

--- visualization/visualization.py
+++ visualization/visualization.py
@@ -1,13 +1,9 @@
 import itertools
 import numpy as np
-from keras.models import load_model
 from matplotlib import pyplot as plt
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from utils.utils import format_data
 
 
-def plot_confusion_matrix(cm, classes, title='Confusion matrix', cmap=plt.cm.Blues):
+def plot_confusion_matrix(cm, classes, title='Confusion matrix', cmap=plt.cm.Blues) -> None:
     tick_marks = np.arange(len(classes))
 
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
@@ -20,30 +16,25 @@ def plot_confusion_matrix(cm, classes, title='Confusion matrix', cmap=plt.cm.Blu
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
+                 horizontalalignment='center',
+                 color='white' if cm[i, j] > thresh else 'black')
 
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.tight_layout()
+    plt.show()
 
 
-image_size = 784
-data = format_data(image_size=image_size)
-images = data['images']
-np_labels_onehot = data['np_labels_onehot']
+def plot_loss_and_accuracy(loss: list, accuracy: list):
+    fig, axs = plt.subplots(nrows=1, ncols=2)
+    fig.suptitle('Loss and accuracy plots')
 
-x_train, x_test, y_train, y_test = train_test_split(images, np_labels_onehot, train_size=55000)
-
-model = load_model('../digits_recognition_model')
-expected_outputs = np.argmax(y_test, axis=1)
-predicted_outputs = np.argmax(model.predict(x_test), axis=1)
-
-class_names = [str(idx) for idx in range(10)]
-cnf_matrix = confusion_matrix(expected_outputs, predicted_outputs)
-np.set_printoptions(precision=2)
-
-plt.figure()
-plot_confusion_matrix(cnf_matrix, classes=class_names, title='Confusion matrix, without normalization')
-
-plt.show()
+    axs[0].plot(loss, 'r-.')
+    axs[1].plot(accuracy, 'g-')
+    axs[0].set_title('loss')
+    axs[1].set_title('accuracy')
+    axs[0].set_ylabel('loss')
+    axs[0].set_xlabel('epochs')
+    axs[1].set_ylabel('accuracy')
+    axs[1].set_xlabel('epochs')
+    plt.show()
