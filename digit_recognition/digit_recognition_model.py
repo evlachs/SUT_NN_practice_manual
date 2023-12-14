85bd8912a512ee88276263f9b43d6b3e46d69729
evlachs <evlach_e@mail.ru>
2023-12-14 20:26:15 +0300

    digit recognition updated

--- digit_recognition/digit_recognition_model.py
+++ digit_recognition/digit_recognition_model.py
@@ -1,21 +1,35 @@
+import numpy as np
 from tensorflow import keras
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
-from utils.utils import format_data
 
+from utils.utils import format_data
+from visualization.visualization import plot_loss_and_accuracy, plot_confusion_matrix
 
 image_size = 28*28
 
 data = format_data(image_size=image_size)
 images = data['images']
-np_labels_onehot = data['np_labels_onehot']
+labels = data['labels']
 
-x_train, x_test, y_train, y_test = train_test_split(images, np_labels_onehot, train_size=55000)
+x_train, x_test, y_train, y_test = train_test_split(images, labels, train_size=55000)
 
 model = keras.Sequential()
 model.add(keras.layers.Dense(input_shape=(image_size,), units=128, activation='relu'))
 model.add(keras.layers.Dense(10, activation='softmax'))
 model.summary()
 model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=10, batch_size=128)
+history = model.fit(x_train, y_train, epochs=50, batch_size=128)
 model.evaluate(x_test, y_test)
 model.save('../model')
+
+expected_outputs = np.argmax(y_test, axis=1)
+predicted_outputs = np.argmax(model.predict(x_test), axis=1)
+
+class_names = [str(idx) for idx in range(10)]
+cnf_matrix = confusion_matrix(expected_outputs, predicted_outputs)
+np.set_printoptions(precision=2)
+
+plot_confusion_matrix(cnf_matrix, classes=class_names, title='Confusion matrix, without normalization')
+
+plot_loss_and_accuracy(history.history['loss'], history.history['accuracy'])
